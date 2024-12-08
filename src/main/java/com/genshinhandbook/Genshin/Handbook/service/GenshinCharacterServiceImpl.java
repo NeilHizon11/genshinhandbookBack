e4e8c0aa91fe2a1f48b6a727786e8a6a819f3303
Neil Jhon Hizon <neilhizon2@gmail.com>
2024-12-08 15:49:24 +0800

    remove unused imports

--- src/main/java/com/genshinhandbook/Genshin/Handbook/service/GenshinCharacterServiceImpl.java
+++ src/main/java/com/genshinhandbook/Genshin/Handbook/service/GenshinCharacterServiceImpl.java
@@ -4,17 +4,9 @@ package com.genshinhandbook.Genshin.Handbook.service;
 import com.genshinhandbook.Genshin.Handbook.model.GenshinCharacter;
 import com.genshinhandbook.Genshin.Handbook.repository.CharacterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Service;
-import jakarta.persistence.*;
-
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
+
 
 @Service
 public class GenshinCharacterServiceImpl implements GenshinCharacterService{
